97e726125958fd84f03852a946243e8aa1ca6fa9
Alexander Redmann <redmann@xplace.de>
2018-02-12 11:47:04 +0100

    split clients into two packages

--- startup.sh
+++ startup.sh
@@ -7,8 +7,8 @@ java -Dspring.application.name=eureka-primary -jar eureka-server/target/eureka-s
 java -Dspring.application.name=eureka-secondary -jar eureka-server/target/eureka-server-1.0.0-SNAPSHOT.jar &
 java -Dspring.application.name=eureka-tertiary -jar eureka-server/target/eureka-server-1.0.0-SNAPSHOT.jar &
 sleep 30
-java -Dspring.application.name=eureka-client-operand -jar client/target/client-1.0.0-SNAPSHOT.jar &
-java -Dspring.application.name=eureka-client-operator -jar client/target/client-1.0.0-SNAPSHOT.jar &
+java -Dspring.application.name=eureka-client-operand -jar client-response/target/client-response-1.0.0-SNAPSHOT.jar &
+java -Dspring.application.name=eureka-client-operator -jar client-response/target/client-response-1.0.0-SNAPSHOT.jar &
 sleep 30
-java -Dspring.application.name=eureka-client-root -jar client/target/client-1.0.0-SNAPSHOT.jar &
+java -Dspring.application.name=eureka-client-root -jar client-request/target/client-request-1.0.0-SNAPSHOT.jar &
 read -p "Press enter to continue"

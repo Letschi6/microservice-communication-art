1ca7b451c9a485f74e9fb8f61f09317107c301e0
Alexander Redmann <redmann@xplace.de>
2018-02-12 11:24:48 +0100

    add ribbon example. add spring boot actuator

--- client/src/main/java/de/art/examples/mc/springcloud/client/controller/RequestController.java
+++ client/src/main/java/de/art/examples/mc/springcloud/client/controller/RequestController.java
@@ -3,7 +3,7 @@ package de.art.examples.mc.springcloud.client.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,16 +13,15 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.net.URI;
-import java.util.List;
 
 @Controller
 @Slf4j
 public class RequestController {
-    private final DiscoveryClient client;
+    private final LoadBalancerClient loadBalancer;
 
     @Autowired
-    public RequestController(DiscoveryClient client) {
-        this.client = client;
+    public RequestController(LoadBalancerClient loadBalancer) {
+        this.loadBalancer = loadBalancer;
     }
 
     @GetMapping("/operation")
@@ -40,13 +39,11 @@ public class RequestController {
     }
 
     private String getPart(String service) {
-        List<ServiceInstance> list = client.getInstances(service);
-        if (list != null && list.size() > 0) {
-            URI uri = list.get(0).getUri();
-            if (uri != null) {
-                log.info("Request service: " + service + " with uri: " + uri.toString());
-                return (new RestTemplate()).getForObject(uri, String.class);
-            }
+        ServiceInstance instance = loadBalancer.choose(service);
+        URI uri = instance.getUri();
+        if (uri != null) {
+            log.info("Request load balanced service: " + service + " with uri: " + uri.toString());
+            return (new RestTemplate()).getForObject(uri, String.class);
         }
         return null;
     }

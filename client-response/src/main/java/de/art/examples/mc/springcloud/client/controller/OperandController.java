f2cc922beedda42a7525f2d4adb7824a8965ede2
Alexander Redmann <redmann@xplace.de>
2018-02-12 13:55:06 +0100

    Add @ResponseBody annotation to client-reponse

--- client-response/src/main/java/de/art/examples/mc/springcloud/client/controller/OperandController.java
+++ client-response/src/main/java/de/art/examples/mc/springcloud/client/controller/OperandController.java
@@ -3,6 +3,7 @@ package de.art.examples.mc.springcloud.client.controller;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -10,7 +11,8 @@ import java.util.concurrent.ThreadLocalRandom;
 @Profile("!operator")
 public class OperandController {
     @GetMapping("/")
-    public String getOperand() {
+    public @ResponseBody
+    String getOperand() {
         return "" + ThreadLocalRandom.current().nextInt(0, 101);
     }
 }

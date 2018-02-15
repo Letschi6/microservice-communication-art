f0d632067bac129f7f1ab8619ec81eed5d0c2598
Alexander Redmann <redmann@xplace.de>
2018-02-15 12:14:32 +0100

    expose only request endpoints via swagger

---
+++ client-response/src/main/java/de/art/examples/mc/springcloud/client/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package de.art.examples.mc.springcloud.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Created by redmann on 07.03.17.
+ */
+@Configuration
+public class SwaggerConfig
+{
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("default").select()
+                .apis(RequestHandlerSelectors.basePackage("de.art.examles")).build();
+    }
+}

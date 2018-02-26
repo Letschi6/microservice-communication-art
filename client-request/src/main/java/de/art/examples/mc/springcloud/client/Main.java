a2045f4c77a128d2e3780897c5896889382a0917
Alexander Redmann <redmann@xplace.de>
2018-02-26 09:30:02 +0100

    remove lombok

--- client-request/src/main/java/de/art/examples/mc/springcloud/client/Main.java
+++ client-request/src/main/java/de/art/examples/mc/springcloud/client/Main.java
@@ -1,12 +1,12 @@
 package de.art.examples.mc.springcloud.client;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -18,8 +18,9 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
-@Slf4j
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         log.info("Starting spring cloud feign client");
         SpringApplication.run(Main.class, args);

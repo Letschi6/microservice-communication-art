ba59ab4e29a2919d4f38b25f3cafc00f95660d3c
Alexander Redmann <redmann@xplace.de>
2018-02-26 09:23:42 +0100

    remove lombok

--- client-request/src/main/java/de/art/examples/mc/springcloud/client/Main.java
+++ client-request/src/main/java/de/art/examples/mc/springcloud/client/Main.java
@@ -1,6 +1,7 @@
 package de.art.examples.mc.springcloud.client;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,8 +14,9 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@Slf4j
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         log.info("Starting spring cloud feign client");
         SpringApplication.run(Main.class, args);
--- client-request/src/main/java/de/art/examples/mc/springcloud/client/controller/RequestController.java
+++ client-request/src/main/java/de/art/examples/mc/springcloud/client/controller/RequestController.java
@@ -1,8 +1,10 @@
 package de.art.examples.mc.springcloud.client.controller;
 
+import de.art.examples.mc.springcloud.client.Main;
 import de.art.examples.mc.springcloud.client.feign.OperandFeignInterface;
 import de.art.examples.mc.springcloud.client.feign.OperatorFeignInterface;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,9 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 @Controller
-@Slf4j
 public class RequestController {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     private final OperandFeignInterface operandFeignInterface;
     private final OperatorFeignInterface operatorFeignInterface;
 

407632aef237c73fbabeebf037c40b31d6c24594
Alexander Redmann <redmann@xplace.de>
2018-02-26 09:26:51 +0100

    remove lombok

--- turbine-server/src/main/java/de/art/examples/mc/springcloud/turbine/Main.java
+++ turbine-server/src/main/java/de/art/examples/mc/springcloud/turbine/Main.java
@@ -1,11 +1,10 @@
 package de.art.examples.mc.springcloud.turbine;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
@@ -17,8 +16,9 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableTurbine
-@Slf4j
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         log.info("Starting spring cloud turbine server");
         SpringApplication.run(Main.class, args);

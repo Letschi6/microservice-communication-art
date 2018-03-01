d3a1fe52b376736ac8d830967889b5ea72fb2a39
Alexander Redmann <redmann@xplace.de>
2018-03-01 16:20:37 +0100

    add Hystrix command

--- client-request/src/main/java/de/art/examples/mc/springcloud/client/feign/OperatorFeignInterface.java
+++ client-request/src/main/java/de/art/examples/mc/springcloud/client/feign/OperatorFeignInterface.java
@@ -1,5 +1,7 @@
 package de.art.examples.mc.springcloud.client.feign;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "eureka-client-operator", fallback = OperatorFeignFallback.class)
 public interface OperatorFeignInterface {
     @GetMapping("/")
+    @HystrixCommand(commandKey = "key", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds"
+                    , value = "30000")})
     String getOperation();
 }
 

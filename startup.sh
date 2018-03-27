84ac3c82784083b690309acc06e4d7c548160b1d
Alexander Redmann <redmann@xplace.de>
2018-03-27 17:04:37 +0200

    use parent pom config.

--- startup.sh
+++ startup.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env bash
-#mvn clean package
-trap 'kill $(jobs -p)' SIGINT
-~/tools/kafka_2.12-1.0.0/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.0/config/zookeeper.properties &
-sleep 10
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server2.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server3.properties &
-sleep 10
-
-read -p "Press enter to continue"
+mvn clean package

ee5442e1f5403b18793696dac4446ad02dfe439b
Alexander Redmann <redmann@xplace.de>
2018-03-28 13:29:07 +0200

    use kafka 1.0.1 use Swagger 2.8.0

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 trap 'kill $(jobs -p)' SIGINT
-~/tools/kafka_2.12-1.0.0/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.0/config/zookeeper.properties &
+~/tools/kafka_2.12-1.0.1/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.1/config/zookeeper.properties &
 sleep 10
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server2.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server3.properties &
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server.properties &
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server2.properties &
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server3.properties &
 sleep 20
 ~/tools/confluent-4.0.0/bin/schema-registry-start  ~/tools/confluent-4.0.0/etc/schema-registry/schema-registry.properties
 sleep 10

7e37d738046023c32da57b7c186426f3c0251895
Alexander Redmann <redmann@xplace.de>
2018-03-28 13:31:22 +0200

    use kafka 1.0.1 use Swagger 2.8.0

--- startup.sh
+++ startup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-#mvn clean package
+mvn clean package
 trap 'kill $(jobs -p)' SIGINT
-~/tools/kafka_2.12-1.0.0/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.0/config/zookeeper.properties &
+~/tools/kafka_2.12-1.0.1/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.1/config/zookeeper.properties &
 sleep 10
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server2.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server3.properties &
-sleep 10
-
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server.properties &
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server2.properties &
+~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server3.properties &
+sleep 20
+java -jar producer/target/producer-1.0.0-SNAPSHOT.jar
+java -jar producer/target/consumer-1.0.0-SNAPSHOT.jar
+java -jar producer/target/consumer-1.0.0-SNAPSHOT.jar -Dserver.port=8201
 read -p "Press enter to continue"

d3eca7aadc6d07a1fa7c3d429e6e8ccdb72ae26f
Alexander Redmann <redmann@xplace.de>
2018-03-29 11:52:32 +0200

    use swagger 2.8.0. remove kafka from startup

--- startup.sh
+++ startup.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-#mvn clean package
+mvn clean package
 trap 'kill $(jobs -p)' SIGINT
-~/tools/kafka_2.12-1.0.0/bin/zookeeper-server-start.sh ~/tools/kafka_2.12-1.0.0/config/zookeeper.properties &
-sleep 10
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server2.properties &
-~/tools/kafka_2.12-1.0.0/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.0/config/server3.properties &
-sleep 10
-~/tools/confluent-4.0.0/bin/schema-registry-start  ~/tools/confluent-4.0.0/etc/schema-registry/schema-registry.properties
-sleep 10
-
+java -jar classic/target/classic-1.0.0-SNAPSHOT.jar &
+java -jar graphql-service/target/graphql-service-1.0.0-SNAPSHOT.jar &
 read -p "Press enter to continue"

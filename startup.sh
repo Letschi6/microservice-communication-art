331b9a2ce6b128eddec6afafbdd02514688f7ebd
Alexander Redmann <redmann@xplace.de>
2018-04-05 13:03:56 +0200

    remove spring cloud dependency import from pom fix startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ sleep 10
 ~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server2.properties &
 ~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server3.properties &
 sleep 20
-java -jar producer/target/producer-1.0.0-SNAPSHOT.jar
-java -jar producer/target/consumer-1.0.0-SNAPSHOT.jar
-java -jar producer/target/consumer-1.0.0-SNAPSHOT.jar -Dserver.port=8201
+java -jar producer/target/producer-1.0.0-SNAPSHOT.jar &
+java -jar consumer/target/consumer-1.0.0-SNAPSHOT.jar &
+java -Dserver.port=8201 -jar consumer/target/consumer-1.0.0-SNAPSHOT.jar &
 read -p "Press enter to continue"

df5cdb3f98d9af482ea467acf9d6e196fe396445
Alexander Redmann <redmann@xplace.de>
2018-04-17 13:25:49 +0200

    fix resolver

--- graphql-service/src/main/java/de/art/examples/mc/graphql/producer/graphql/resolver/StockResolver.java
+++ graphql-service/src/main/java/de/art/examples/mc/graphql/producer/graphql/resolver/StockResolver.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StockResolver implements GraphQLResolver<Article> {
+public class StockResolver implements GraphQLResolver<Stock> {
     private final ArticleRepository articleRepository;
 
     @Autowired

07d99b8e5ad6d766501d9b360567737050cf9a15
Alexander Redmann <redmann@xplace.de>
2018-04-26 10:14:35 +0200

    use new conluent version

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ sleep 10
 ~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server2.properties &
 ~/tools/kafka_2.12-1.0.1/bin/kafka-server-start.sh ~/tools/kafka_2.12-1.0.1/config/server3.properties &
 sleep 20
-~/tools/confluent-4.0.0/bin/schema-registry-start  ~/tools/confluent-4.0.0/etc/schema-registry/schema-registry.properties
+~/tools/confluent-4.1.0/bin/schema-registry-start  ~/tools/confluent-4.1.0/etc/schema-registry/schema-registry.properties
 sleep 20
 mvn clean package
 
